712e437458a90eb5d5ca9bb4025ef6a47ea9d419
avieira-42 <vieiramendesalex@gmail.com>
2025-04-15 00:37:03 +0100

    updated libft

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: avieira- <avieira-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 18:52:15 by avieira-          #+#    #+#             */
-/*   Updated: 2025/04/14 20:07:43 by avieira-         ###   ########.fr       */
+/*   Updated: 2025/04/14 22:49:12 by jesusoncrac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t			i;
-	size_t			len_s;
-	unsigned char	*str;
-	char			*substr;
+	size_t	i;
+	size_t	len_s;
+	char	*substr;
 
 	i = 0;
 	len_s = ft_strlen(s);
-	str = (unsigned char *) s;
-	substr = malloc (len +1);
+	substr = malloc (len + 1);
 	if (!substr)
 		return (0);
-	while (i < len && i < len_s - 1)
+	while (i < len && start + i < len_s)
 	{
 		substr[i] = s[start + i];
 		i++;
