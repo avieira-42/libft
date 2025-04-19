9f96c06ee75d0cabad15a9dae330c4fe00a3a7b6
avieira-42 <vieiramendesalex@gmail.com>
2025-04-19 02:57:09 +0100

    updated libft.a

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: avieira- <avieira-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 19:10:35 by avieira-          #+#    #+#             */
-/*   Updated: 2025/04/17 20:18:35 by avieira-         ###   ########.fr       */
+/*   Updated: 2025/04/19 02:54:36 by jesusoncrac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*lsd;
+	void	*app;
 	t_list	*tmp;
-	void	*pqvp;
+	t_list	*new_lst;
 
-	lsd = NULL;
+	new_lst = NULL;
 	while (lst)
 	{
-		pqvp = f(lst->content);
-		tmp = ft_lstnew(pqvp);
+		app = f(lst->content);
+		tmp = ft_lstnew(app);
 		if (!tmp)
 		{
-			free(pqvp);
-			ft_lstclear(&lsd, del);
+			free(app);
+			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&lsd, tmp);
+		ft_lstadd_back(&new_lst, tmp);
 		lst = lst->next;
 	}
-	return (lsd);
+	return (new_lst);
 }
